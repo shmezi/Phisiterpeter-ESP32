698147a2b64d3adb45c11ed1afafac8c069d9190
Ezra Golombek <67468040+shmezi@users.noreply.github.com>
2026-01-22 17:38:00 +0200

    My dumbass forgot to convert to integers from floats, when we moved from ints to floats for numbers. Thanks me..

--- src/expressions/game/DigitalReadExpression.cpp
+++ src/expressions/game/DigitalReadExpression.cpp
@@ -7,18 +7,27 @@
 #include <driver/gpio.h>
 #include <hal/gpio_types.h>
 
+#include "Utils.h"
 #include "expressions/value/BooleanExpression.h"
 
 std::string DigitalReadExpression::expressionName() {
     return "digitalRead";
 }
 
+bool hasRun = false;
+
 std::shared_ptr<Expression> DigitalReadExpression::interpret(std::shared_ptr<Scope> scope) {
     auto expression = dynamic_cast<NumberExpression *>(pin->interpret(scope).get());
+    const  auto pinNumber = static_cast<int>(expression->contents);
 
-    const auto gpio = static_cast<gpio_num_t>(expression->contents);
-    gpio_set_direction(gpio, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(gpio,GPIO_PULLUP_ONLY);
+    const auto gpio = static_cast<gpio_num_t>(pinNumber);
+    if (!hasRun) {
+        debug::log("Configured pin " + std::to_string(pinNumber) + " as an input!");
+        gpio_set_direction(gpio, GPIO_MODE_INPUT);
+        gpio_set_pull_mode(gpio,GPIO_PULLUP_ONLY);
+        hasRun = true;
+    }
+
     return std::make_unique<BooleanExpression>(gpio_get_level(gpio)==1);
 }
 
