e0a9d103cfcd8f73a08497cfbec6cde7b42a69d6
Ezra Golombek <67468040+shmezi@users.noreply.github.com>
2026-06-11 09:31:26 +0300

    Fixed logger endl

--- src/logging/Logger.cpp
+++ src/logging/Logger.cpp
@@ -14,22 +14,22 @@ void Logger::log(const std::string &contents) {
 
 void Logger::warn(const std::string &contents) {
     std::cout << (apply(Color::BOLD) + apply(Color::YELLOW) + "[WARN] " + apply(Color::RESET) +
-                  apply(Color::BRIGHT_WHITE) + contents);
+                  apply(Color::BRIGHT_WHITE) + contents) << std::endl;
     debug::showColor(debug::WARN);
 }
 
 void Logger::bootMessage(const std::string &contents) {
     std::cout << (apply(Color::BOLD) + apply(Color::BRIGHT_BLUE) + "[BOOT] " + apply(Color::RESET) +
-                  apply(Color::BRIGHT_WHITE) + contents);
+                  apply(Color::BRIGHT_WHITE) + contents) << std::endl;
 }
 
 void Logger::configuration(const std::string &contents) {
     std::cout << (apply(Color::BOLD) + apply(Color::BRIGHT_BLUE) + "[CONFIG] " + apply(Color::RESET) +
-                  apply(Color::BRIGHT_WHITE) + contents);
+                  apply(Color::BRIGHT_WHITE) + contents) << std::endl;
 }
 
 void Logger::error(const std::string &contents) {
     std::cout << (apply(Color::BOLD) + apply(Color::RED) + "[ERROR] " + apply(Color::RESET) + apply(Color::BRIGHT_WHITE)
-                  + contents);
+                  + contents) << std::endl;
     debug::showColor(debug::INTERPRET_ERROR);
 }
